56635c9d934f398b186dbe8b512c230d42667135
Peyton Hollenback <145724342+phollenback@users.noreply.github.com>
2023-12-10 23:22:02 -0700

    Add files via upload

---
+++ AdminApp/salable/Armor.java
@@ -0,0 +1,39 @@
+package Sales;
+
+import com.fasterxml.jackson.annotation.JsonTypeName;
+
+/**
+ * Type of saleable that inherits saleable class
+ */
+//@JsonTypeName("armor")
+public class Armor extends Saleable {
+	
+	
+	/**
+	 * Inherits Saleable
+	 * @param Id Id of salable
+	 * @param name OF saleable
+	 * @param description of Saleable
+	 * @param price Of Saleable
+	 * @param quantity of Saleable
+	 */
+	public Armor(int Id, String name, String description, float price, int quantity)
+	{
+		super(Id, name, description, price, quantity);
+	}
+	
+	/**
+	 * Default Constructor
+	 */
+	public Armor()
+	{
+		super();
+	}
+
+	
+
+	
+	
+	
+	
+}
---
+++ AdminApp/salable/Health.java
@@ -0,0 +1,33 @@
+package Sales;
+
+import com.fasterxml.jackson.annotation.JsonTypeName;
+
+/**
+ * Type of saleable that inherits saleable class
+ */
+//@JsonTypeName("health")
+public class Health extends Saleable{
+	
+	
+	/**
+	 * Inherits Saleable
+	 * @param Id Id of salable
+	 * @param name of saleable
+	 * @param description of saleable
+	 * @param price of saleable
+	 * @param quantity of saleable
+	 */
+	public Health(int Id, String name, String description, float price, int quantity)
+	{
+		super(Id, name, description, price, quantity);
+	}
+	
+	/**
+	 * Default constructor
+	 */
+	public Health()
+	{
+		super();
+	}
+
+}
